9e2f99452f9cb0a188a96f1e99b7a880286357c3
EricNorrwing <eric.norrwing@gmail.com>
2025-04-28 16:42:00 +0200

    some minor updates to test PR to main

--- JobApplicationHandler.Server/Services/JobApplicationService.cs
+++ JobApplicationHandler.Server/Services/JobApplicationService.cs
@@ -1,15 +1,16 @@
 ﻿using JobApplicationHandler.Contracts.JobApplications;
+using JobApplicationHandler.Server.Repositories;
 
 namespace JobApplicationHandler.Server.Services;
 
 public interface IJobApplicationService
 {
-    public Task<IEnumerable<JobApplication>> GetJobApplicationByIdAsync();
+    public Task<IEnumerable<JobApplication>> GetJobApplicationByIdAsync(string id);
 }
 public class JobApplicationService(JobApplicationRepository jobApplicationRepository): IJobApplicationService
 {
-    public async Task<IEnumerable<JobApplication>> GetJobApplicationByIdAsync()
+    public async Task<IEnumerable<JobApplication>> GetJobApplicationByIdAsync(string id)
     {
-        
+        return await jobApplicationRepository.GetJobApplicationByIdAsync(id);
     }
 }
