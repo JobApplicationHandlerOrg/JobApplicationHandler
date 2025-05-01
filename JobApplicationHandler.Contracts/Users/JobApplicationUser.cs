5c51e461e849c89f07f68d74f7c21b8a9e8db323
EricNorrwing <eric.norrwing@gmail.com>
2025-05-01 13:37:31 +0200

    fixed a bunch of stuff relating to git

--- JobApplicationHandler.Contracts/Users/JobApplicationUser.cs
+++ JobApplicationHandler.Contracts/Users/JobApplicationUser.cs
@@ -2,11 +2,12 @@
 
 public class JobApplicationUser
 {
-    public required string Id { get; set; }
-    public required string Username { get; set; }
-    public required string Password { get; set; }
-    public UserRoles UserRoles { get; set; }
-        
+    public required string Id { get; init; }
+    public required string Username { get; init; }
+    public required string Password { get; init; }
+    public UserRoles UserRoles { get; init; }
+    public DateTime RegistrationDate { get; init; } = DateTime.Now;
+    public DateTime? LastLoginDate { get; set; } = DateTime.Now;
     
-    
+
 }
--- JobApplicationHandler.Server/Configurations/DbContexts/DbContextRegistration.cs
+++ JobApplicationHandler.Server/Configurations/DbContexts/DbContextRegistration.cs
@@ -1,12 +1,25 @@
-﻿namespace JobApplicationHandler.Server.Configurations.DBContexts;
+﻿using JobApplicationHandler.Server.Repositories.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobApplicationHandler.Server.Configurations.DBContexts;
 
 public static class DbContextRegistration
 {
     public static IServiceCollection AddDbContextRegistration(this IServiceCollection services, IConfiguration configuration)
     {
+        var jobAppUserDb =
+            configuration.GetConnectionString("JobApplicationDb")
+            ?? throw new InvalidOperationException("Connection string"
+                                                   + "'JobAppUserDb' not found.");
+        var jobApplicationDb =
+            configuration.GetConnectionString("JobAppUserDb")
+            ?? throw new InvalidOperationException("Connection string"
+                                                   + "'JobApplicationDb' not found.");
+       
         services.AddDbContext<JobApplicationDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("JobAppUserDb")));
-        services.AddDbContext<JobAppUserDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("JobApplicationDb")) );
+            options.UseSqlServer(configuration.GetConnectionString(jobAppUserDb)));
+        services.AddDbContext<JobAppUserDbContext>(options => 
+            options.UseSqlServer(configuration.GetConnectionString(jobApplicationDb)) );
         
         return services;
     }
