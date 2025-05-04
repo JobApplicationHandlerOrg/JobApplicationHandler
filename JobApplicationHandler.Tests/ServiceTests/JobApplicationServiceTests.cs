d3952528ed0e9fc126b08ff52c6682b4b98adc88
EricNorrwing <eric.norrwing@gmail.com>
2025-05-04 12:46:27 +0200

    added a simple test, updated pipeline

---
+++ JobApplicationHandler.Tests/ServiceTests/JobApplicationServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using JobApplicationHandler.Contracts.JobApplications;
+using JobApplicationHandler.Server.Repositories;
+using JobApplicationHandler.Server.Services;
+using Moq;
+
+namespace JobApplicationHandler.Tests.ServiceTests;
+
+public class JobApplicationServiceTests
+{
+    private readonly Mock<IJobApplicationRepository> _mockRepo;
+    private readonly JobApplicationService _service;
+
+    public JobApplicationServiceTests()
+    {
+        _mockRepo = new Mock<IJobApplicationRepository>();
+        _service = new JobApplicationService(_mockRepo.Object);
+    }
+
+    [Fact]
+    public async Task AssertThatJobApplicationExists()
+    {
+        // Arrange
+        const string fakeId = "123";
+        var expectedApplications = new List<JobApplication>
+        {
+            new JobApplication {
+                CompanyName = "Company",
+                JobTitle = ".Net Software Developer",
+                ApplicationUrl = "ApplicationUrl.com",
+            }
+        };
+        _mockRepo.Setup(repo => repo.GetJobApplicationByIdAsync(fakeId))
+            .ReturnsAsync(expectedApplications);
+
+        // Act
+        var result = await _service.GetJobApplicationByIdAsync(fakeId);
+
+        // Assert
+        Assert.Equal(".Net Software Developer", result.First().JobTitle);
+    }
+}
