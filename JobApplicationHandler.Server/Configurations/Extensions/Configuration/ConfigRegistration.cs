af61d96a1617cd8d2b3d04abb72350b5bd06a9c1
EricNorrwing <eric.norrwing@gmail.com>
2025-05-04 13:31:13 +0200

    updates to workflows, adding secrets to flow and locally with dotnet secrets

---
+++ JobApplicationHandler.Server/Configurations/Extensions/Configuration/ConfigRegistration.cs
@@ -0,0 +1,19 @@
+﻿namespace JobApplicationHandler.Server.Configurations.Extensions.Configuration;
+
+public static class ConfigRegistration
+{
+    public static IConfigurationBuilder AddDConfigurationSettings(this IConfigurationBuilder configuration, IHostEnvironment env)
+    {
+        configuration
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
+        
+        if (env.IsDevelopment())
+        {
+            configuration.AddUserSecrets<Program>();
+        }
+        
+        return configuration;
+    }
+}
