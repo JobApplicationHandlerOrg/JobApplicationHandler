4fbed3495239485cec3ece6f3e3e9ec430a087e7
EricNorrwing <eric.norrwing@gmail.com>
2025-05-11 14:30:49 +0200

    updated the handler to follow a different approach

--- JobApplicationHandler.Server/Infrastructure/Middleware/GlobalExceptionHandler.cs
+++ JobApplicationHandler.Server/Infrastructure/Middleware/GlobalExceptionHandler.cs
@@ -3,22 +3,39 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JobApplicationHandler.Server.Infrastructure.Middleware;
 
-public class GlobalExceptionHandler: IExceptionHandler
+[Serializable]
+public class ProblemException(string error, string errorMessage) : Exception(errorMessage)
+{
+    public string Error { get; } = error;
+    public string ErrorMessage { get; } = errorMessage;
+}
+
+public class GlobalExceptionHandler(
+    ILogger<GlobalExceptionHandler> logger,
+    IProblemDetailsService problemDetailsService) : IExceptionHandler
 {
     public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
     {
-        
-        //TODO: Log something
+        if (exception is not ProblemException problemException)
+            return false; 
 
         var problemDetails = new ProblemDetails
         {
-            Detail = exception.Message,
+            Detail = problemException.ErrorMessage,
             Status = StatusCodes.Status500InternalServerError,
-            Type = "/error/500",
-            Title = "This is a test error",
+            Type = "/error/internal",
+            Title = problemException.Error
         };
-        httpContext.Response.StatusCode = problemDetails.Status.Value;
-        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
-        return true;
+
+        logger.LogError(
+            "An exception has occurred: {Exception}. Message: {Message}. Request Path: {RequestPath}",
+            exception, exception.Message, httpContext.Request.Path);
+
+        return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            ProblemDetails = problemDetails
+        });
     }
+
 }
